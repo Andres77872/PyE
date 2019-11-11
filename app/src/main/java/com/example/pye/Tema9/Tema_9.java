64cc9d632c9cae74d886bb189722fae527097055
andres <andreslamosk124@hotmail.com>
2019-11-11 01:56:31 -0600

    Avance 9.1

--- app/src/main/java/com/example/pye/Tema9/Tema_9.java
+++ app/src/main/java/com/example/pye/Tema9/Tema_9.java
@@ -44,6 +44,7 @@ public class Tema_9 extends AppCompatActivity {
                     Arrays.sort(Entrada);
 
                     ArrayList<Entry> values = new ArrayList<>();
+                    ArrayList<Entry> values2 = new ArrayList<>();
                     LineChart GR = findViewById(R.id.chart_T9_Grafica);
                     GR.setTouchEnabled(true);
                     GR.setPinchZoom(true);
@@ -58,12 +59,12 @@ public class Tema_9 extends AppCompatActivity {
                         Mxy += Integer.parseInt(D[0]) * Integer.parseInt(D[1]);
                     }
 
-                    LineDataSet lineDataSet = new LineDataSet(values, "DATOS");
+                    LineDataSet lineDataSet = new LineDataSet(values, "Datos de dispersion");
                     lineDataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
                     lineDataSet.setHighlightEnabled(true);
                     lineDataSet.setLineWidth(2);
-                    //lineDataSet.setColor(getColor("defaultBlue"));
-                    //lineDataSet.setCircleColor(getColor("defaultOrange"));
+                    lineDataSet.setColor(Color.BLUE);
+                    lineDataSet.setCircleColor(Color.BLUE);
                     lineDataSet.setCircleRadius(6);
                     lineDataSet.setCircleHoleRadius(3);
                     lineDataSet.setDrawHighlightIndicators(true);
@@ -71,7 +72,35 @@ public class Tema_9 extends AppCompatActivity {
                     lineDataSet.setValueTextSize(12);
                     //lineDataSet.setValueTextColor(getColor("primaryDark"));
 
-                    LineData lineData = new LineData(lineDataSet);
+                    int N = Entrada.length;
+
+                    float m = (float) (((N * Mxy) - (Mx * My)) / ((N * Mx2) - Math.pow(Mx, 2))),
+                            b = (float) (((My * Mx2) - (Mx * Mxy)) / ((N * Mx2) - Math.pow(Mx, 2)));
+
+                    ((TextView) findViewById(R.id.lbl_T9_m)).setText(String.format("m = %s", ((int) (m * 1000)) / 1000.0));
+                    ((TextView) findViewById(R.id.lbl_T9_b)).setText(String.format("b = %s", ((int) (b * 1000)) / 1000.0));
+
+
+                    for (String Dato : Entrada) {
+                        String[] D = Dato.split(";");
+                        values2.add(new Entry(Float.parseFloat(D[0]), m * Float.parseFloat(D[0]) + b));
+                    }
+
+
+                    LineDataSet lineDataSet2 = new LineDataSet(values2, "Recta de regresion");
+                    lineDataSet2.setAxisDependency(YAxis.AxisDependency.LEFT);
+                    lineDataSet2.setHighlightEnabled(true);
+                    lineDataSet2.setLineWidth(2);
+                    lineDataSet2.setColor(Color.RED);
+                    lineDataSet2.setCircleColor(Color.RED);
+                    lineDataSet2.setCircleRadius(6);
+                    lineDataSet2.setCircleHoleRadius(3);
+                    lineDataSet2.setDrawHighlightIndicators(true);
+                    lineDataSet2.setHighLightColor(Color.RED);
+                    lineDataSet2.setValueTextSize(12);
+                    //lineDataSet.setValueTextColor(getColor("primaryDark"));
+
+                    LineData lineData = new LineData(lineDataSet, lineDataSet2);
 
                     GR.getDescription().setText("Recta de regresion");
                     GR.getDescription().setTextSize(12);
@@ -83,14 +112,6 @@ public class Tema_9 extends AppCompatActivity {
                     GR.getXAxis().setLabelCount(lineDataSet.getEntryCount());
                     GR.setData(lineData);
 
-
-                    int N = Entrada.length;
-
-                    double m = ((N * Mxy) - (Mx * My)) / ((N * Mx2) - Math.pow(Mx, 2)),
-                            b = ((My * Mx2) - (Mx * Mxy)) / ((N * Mx2) - Math.pow(Mx, 2));
-
-                    ((TextView) findViewById(R.id.lbl_T9_m)).setText(String.format("m = %s", ((int) (m * 1000)) / 1000.0));
-                    ((TextView) findViewById(R.id.lbl_T9_b)).setText(String.format("b = %s", ((int) (b * 1000)) / 1000.0));
 
                     Mx /= N;
                     My /= N;
